c3fc845b9c3234ecec061d70174a85c5d4b2d0f3
Anton Sannikov <anton.sannikov@kawa-commerce.com>
2021-10-12 18:12:53 +0200

    Init

---
+++ Readme.md
@@ -0,0 +1,42 @@
+# One click install markshust/docker-magento 
+
+
+This is a simple bash script, which helps to install magento project in one click.
+
+It works for existing project only. You have to set the git source to the project as a parameter.
+
+Or you can modify install.sh script depending on your purpose.
+
+## How to run
+
+1. Put auth.json file into the root path where you are going to run install.sh.
+
+2. (Optional) You can put your env.php file into the root folder either. Otherwies, default env.php will be used. Do not forgen to copy the mysql data from default env.php.
+
+3. Be sure that ~/.ssh/id_rsa exists
+
+4. Download script into the root folder: `curl -O https://raw.githubusercontent.com/asannikov/markshust-docker-magento-installer/main/install.sh`
+
+5. Replace the command parameters and run it `sh install.sh magento2.dev http://path.to.git.repo path/to/dump/magento.sql`
+
+## Extra features
+- optimized for working on Mac OS!
+
+- it prepares elasticsearch and DB configuration
+
+- you can use xdebug using cli out of box
+
+- you can enable xdebug profile mode. Use bin/xdebug-profile status/profile/disable/toggle. 
+
+NOTE: you have to disable it using bin/xdebug-profile, not by bin/xdebug! Otherwise you have to clean up php.ini file manually.
+
+- fixed problem with id_rsa key on start.
+
+
+## How to use xdebug in CLI
+
+`bin/bash` and then inside container run `export XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_host=your.domain remote_port=9000 remote_connect_back=0"`
+
+do not forget to replace "your.domain"!
+
+more info here: https://www.jetbrains.com/help/phpstorm/debugging-a-php-cli-script.html
---
+++ env.php
@@ -0,0 +1,152 @@
+<?php
+return [
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'indexer' => [
+        'batch_size' => [
+            'catalogsearch_fulltext' => [
+                'partial_reindex' => 10,
+                'mysql_get' => 10,
+                'elastic_save' => 10
+            ]
+        ]
+    ],
+    'db' => [
+        'connection' => [
+            'indexer' => [
+                'host' => 'db',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => 'magento',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'persistent' => null
+            ],
+            'default' => [
+                'host' => 'db',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => 'magento',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ],
+        'table_prefix' => ''
+    ],
+    'crypt' => [
+        'key' => 'crypt_key:crypt_key:crypt_key:12'
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'redis',
+        'redis' => [
+            'host' => 'redis',
+            'port' => '6379',
+            'password' => '',
+            'timeout' => '2.5',
+            'persistent_identifier' => '',
+            'database' => '2',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '1',
+            'max_concurrency' => '100',
+            'break_after_frontend' => '5',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '0',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000',
+            'sentinel_master' => '',
+            'sentinel_servers' => '',
+            'sentinel_connect_retries' => '5',
+            'sentinel_verify_master' => '0'
+        ]
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69d_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => 'redis',
+                    'database' => '0',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '1',
+                    'compression_lib' => ''
+                ]
+            ],
+            'page_cache' => [
+                'id_prefix' => '69d_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => 'redis',
+                    'database' => '1',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '1',
+                    'compression_lib' => ''
+                ]
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'full_page' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'target_rule' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1,
+        'wp_gtm_categories' => 1
+    ],
+    'install' => [
+        'date' => 'Tue, 29 Jun 2021 14:38:28 +0000'
+    ],
+    'dev' => [
+        'debug' => [
+            'debug_logging' => 1
+        ]
+    ],
+    'backend' => [
+        'frontName' => 'admin'
+    ]
+];
